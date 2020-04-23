bb4a4c148102e13e23baab36a5a7e5ed888ae5ec
Radosław <radoslaw.biesek@gmail.com>
2020-04-23 21:07:21 +0200

    Gatsby tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
 import React from 'react';
 
-export default () => <div>Hello Frontend Masters!</div>;
+import Layout from '../components/layout';
+import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
+import Insta from '../components/insta';
+
+import usePosts from '../hooks/usePosts';
+
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <>
+      <Hero />
+      <Layout>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+        <Insta />
+      </Layout>
+    </>
+  );
+};
